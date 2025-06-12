d69e4718e99767348db96dda9eb8e46a9b7d2e7b
Sherlock Nguyen <quocx2003@gmail.com>
2025-06-12 11:42:21 +0700

    update webhooks.js

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,6 +1,6 @@
 
 import { Webhook } from "svix";
-import User from "../models/User";
+import User from "../models/User.js";
 
 // API Controller Function to Manage Clerk User with database
 export const clerkWebhooks = async (req, res) => {
